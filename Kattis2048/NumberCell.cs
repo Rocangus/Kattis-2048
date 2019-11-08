c1ef0743091d184e6c8e7969f663b0400302bff3
Rocangus <hunter_sniper112@hotmail.com>
2019-11-08 11:53:38 +0100

    Finished and Accepted

--- Kattis2048/NumberCell.cs
+++ Kattis2048/NumberCell.cs
@@ -6,9 +6,21 @@ namespace Kattis2048
 {
     class NumberCell
     {
+        private static NumberCell empty;
         public bool Merged { get; private set; } = false;
         public Tuple<int, int> Position { get; private set; }
         public int Value { get; private set; }
+
+        public static NumberCell EmptyCell 
+        { 
+            get {
+                if (empty == null)
+                {
+                    empty = new NumberCell(0, Tuple.Create(0, 0));
+                }
+                return empty;
+            } 
+        }
 
         public NumberCell(int value, Tuple<int, int> position)
         {
@@ -16,7 +28,12 @@ namespace Kattis2048
             Position = position;
         }
 
-        public NumberCell Merge(NumberCell merger, NumberCell mergee)
+        public void SetPosition(Tuple<int, int> newPos)
+        {
+            Position = newPos;
+        }
+
+        public static NumberCell Merge(NumberCell merger, NumberCell mergee)
         {
             merger.Value += mergee.Value;
             merger.Merged = true;
--- Kattis2048/Program.cs
+++ Kattis2048/Program.cs
@@ -2,9 +2,11 @@
 
 namespace Kattis2048
 {
-    class Program
+    public enum Direction { left, up, right, down}
+    public class Program
     {
-        static int rows, columns;
+        static int rows, columns, actions;
+        static NumberCell[,] cells;
         static void Main(string[] args)
         {
             rows = columns = 4;
@@ -14,8 +16,13 @@ namespace Kattis2048
                 cellLines[i] = Console.ReadLine();
             }
             var moveDirection = int.Parse(Console.ReadLine());
-            var cells = PopulateCellArrays(cellLines);
-            cells = processMove(cells, moveDirection);
+            cells = PopulateCellArrays(cellLines);
+            do
+            {
+                actions = 0;
+                cells = ProcessMove(cells, moveDirection);
+            } while (actions > 0);
+            PrintResults();
         }
 
         private static NumberCell[,] PopulateCellArrays(string[] cellLines)
@@ -28,14 +35,108 @@ namespace Kattis2048
                 {
                     var position = Tuple.Create(y, x);
                     cells[y, x] = new NumberCell(int.Parse(values[x]), position);
-                } //TODO: Make move methods that make use of y and x and backtrack until the bounds of the multidimensional NumberCell array to move cells after any merging.
+                } 
             }
             return cells;
         }
 
-        private static NumberCell[,] processMove(NumberCell[,] cells, int moveDirection)
+        private static NumberCell[,] ProcessMove(NumberCell[,] cells, int moveDirection)
         {
+            MoveCells((Direction)moveDirection);
+            
             return cells;
+        }
+
+        private static void MoveCells(Direction moveDirection)
+        {
+            Tuple<int, int> moveFactor;
+            switch (moveDirection)
+            {
+                case Direction.left:
+                    moveFactor = Tuple.Create(0, -1);
+                    break;
+                case Direction.up:
+                    moveFactor = Tuple.Create(-1, 0);
+                    break;
+                case Direction.right:
+                    moveFactor = Tuple.Create(0, 1);
+                    break;
+                case Direction.down:
+                    moveFactor = Tuple.Create(1, 0);
+                    break;
+                default:
+                    moveFactor = Tuple.Create(0, 0);
+                    break;
+            }
+            MergeOrMove(moveFactor);
+        }
+
+        private static void MergeOrMove(Tuple<int, int> moveFactor)
+        {
+            int changeY, changeX;
+            changeY = changeX = 1;
+            if (moveFactor.Item1 < 0)
+            {
+                changeY = -1;
+            }
+            if (moveFactor.Item2 < 0)
+            {
+                changeX = -1;
+            }
+            
+            // Process actions "bottom up" to produce expected merge behavior, see Sample 5
+            for (int y = changeY == 1? 3 : 0; InRange(y); y -= changeY)
+            {
+                for (int x = changeX == 1 ? 3 : 0; InRange(x); x -= changeX)
+                {
+                    var cell = cells[y, x];
+                    var pos = cell.Position;
+                    pos = AddPositions(pos, moveFactor);
+                    var newY = pos.Item1;
+                    var newX = pos.Item2;
+                    if (InRange(newY)&&InRange(newX))
+                    {
+                        var nextCell = cells[newY, newX];
+                        if (cells[newY, newX].Value == 0)
+                        {
+                            cells[newY, newX] = cell;
+                            cell.SetPosition(pos);
+                            cells[y, x] = NumberCell.EmptyCell;
+                            actions++;
+                        }
+                        else if (!(cell.Merged || nextCell.Merged) && cell.Value == nextCell.Value)
+                        {
+                            cell = NumberCell.Merge(cell, nextCell);
+                            cells[newY, newX] = cell;
+                            cell.SetPosition(pos);
+                            cells[y, x] = NumberCell.EmptyCell;
+                            actions++;
+                        } 
+                    }
+                }
+            }
+        }
+
+        private static bool InRange(int i)
+        {
+            return i < 4 && 0 <= i;
+        }
+
+        private static Tuple<int, int> AddPositions(Tuple<int, int> tuple1, Tuple<int, int> tuple2)
+        {
+            return Tuple.Create(tuple1.Item1 + tuple2.Item1, tuple1.Item2 + tuple2.Item2);
+        }
+
+        private static void PrintResults()
+        {
+            for (int y = 0; y < rows; y++)
+            {
+                for (int x = 0; x < columns; x++)
+                {
+                    if (x < columns - 1) Console.Write(cells[y,x].Value + " ");
+                    else Console.Write(cells[y, x].Value + "\n");
+                }
+            }
         }
     }
 }
